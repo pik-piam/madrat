8b92b14cddb8547b39977e9a62cde0e0e1a4926a
Jan Dietrich <dietrich@pik-potsdam.de>
2017-07-06 21:32:07 +0200

    added functionality to toolNAreplace

--- R/toolNAreplace.R
+++ R/toolNAreplace.R
@@ -1,18 +1,20 @@
 #' Tool: NA replace
 #' 
-#' Functions removes NAs in x and weight
+#' Functions removes NAs, NaNs and infinite values in x and weight
 #' 
 #' 
 #' @param x data
 #' @param weight aggregation weight
 #' @param replaceby value which should be used instead of NA
+#' @param val.rm vector of values that should in addition be removed in x 
 #' @return a list containing x and weight
-#' @author Benjamin Bodirsky
+#' @author Benjamin Bodirsky, Jan Philipp Dietrich
 #' @seealso \code{\link{calcOutput}}
 #' @export
 #' 
 
-toolNAreplace<-function(x,weight,replaceby=0){
+toolNAreplace<-function(x, weight, replaceby=0, val.rm=NULL){
+  if(!is.null(val.rm)) x[x %in% val.rm] <- NA
   na_remove<-x*weight*0+1
   x<-x*na_remove
   weight<-weight*na_remove
