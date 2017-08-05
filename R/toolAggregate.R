ba5285383338b13826aea9f402327040fd2d978e
Jan Dietrich <dietrich@pik-potsdam.de>
2017-08-05 21:46:23 +0200

    added check for negative values in weight

--- R/toolAggregate.R
+++ R/toolAggregate.R
@@ -25,8 +25,9 @@
 #' A mapping object should contain 2 columns in which each element of x
 #' is mapped to the category it should belong to after (dis-)aggregation
 #' @param weight magclass object containing weights which should be considered
-#' for a weighted aggregation. Please see the "details" section below for more
-#' information
+#' for a weighted aggregation. The provided weight must only contain positive
+#' values, but does not need to be normalized (any positive number>=0 is allowed). 
+#' Please see the "details" section below for more information.
 #' @param from Name of the first column to be used in rel if it is a
 #' mapping (if not set the first or second column will be used). 
 #' @param to Name of the second column to be used in rel if it is a
@@ -59,8 +60,9 @@
 #' toolAggregate(population_magpie,mapping, weight=population_magpie)
 
 toolAggregate <- function(x, rel, weight=NULL, from=NULL, to=NULL, dim=1, partrel=FALSE, rev=2) {
-
+  
   if(!is.null(weight) & rev>=2) {
+    if(any(weight<0)) stop("Negative numbers in weight. Weight must be positive!")
     weight2 <- 1/(toolAggregate(weight, rel, from=from, to=to, dim=dim, partrel=partrel) + 10^-100)
     comment <- getComment(x)
     if(setequal(getItems(weight, dim=dim), getItems(x, dim=dim))) {
@@ -99,7 +101,7 @@ toolAggregate <- function(x, rel, weight=NULL, from=NULL, to=NULL, dim=1, partre
   }  
   
   .toolScale <- function(x,rel,weight,dim=1,comment=NULL) {
-  .calcweightmatrix <- function(weight,rel) {
+    .calcweightmatrix <- function(weight,rel) {
     .tmp <- function(w,rel) {
       #return matrix if weights are all 0 or all NA (meaning that data should be summed up)
       if(all(is.na(w))) return(rel)
@@ -142,124 +144,124 @@ toolAggregate <- function(x, rel, weight=NULL, from=NULL, to=NULL, dim=1, partre
       return(array(apply(weight,nd,.tmp,rel),dim=c(dim(rel),dim(weight)[nd]),dimnames=list(dimnames(rel)[[1]],dimnames(rel)[[2]],dimnames(weight)[[nd[1]]],dimnames(weight)[[nd[2]]])))
     }
   }
-  out <- list()
-  w <- .calcweightmatrix(weight,rel)
-  nd <- which(1:3!=dim)
-  
-  if(dim(w)[4]>1 & dim(w)[4]!=ndata(x)) {
-    #check for possible name collisions in data dimension
-    matches <- sapply(dimnames(w)[[4]],grepl,fulldim(x)[[2]])
-    if(any(colSums(matches)>1)) {
-      similar_subdimensions <- TRUE
-      x <- clean_magpie(x,what = "sets")
-      tmp <- sapply(fulldim(x)[[2]],setequal,dimnames(w)[[4]])
-      subdim2use <- names(tmp)[tmp]
-      if(length(subdim2use)>1) stop("Unclear mapping of weight to data subdimension!")
-      if(length(subdim2use)==0) stop("Could not find data subdimension which matches the given data!")
-    } else {
-      similar_subdimensions <- FALSE      
-    }
-  } else {
-    similar_subdimensions <- FALSE
-  }
-
-  for(y in 1:dim(w)[3]) {
-    tmp <- list()
-    if(dim(w)[3]==1) {
-      if(dim==1) i1 <- 1:dim(x)[2]
-      if(dim==2) i1 <- 1:dim(x)[1]
-      if(dim==3) i1 <- 1:dim(x)[1]
-    } else {
-      i1 <- y
-    }
-    for(d in 1:dim(w)[4]) {      
-      if(dim(w)[4]==1) {
-        if(dim==1) i2 <- 1:dim(x)[3]
-        if(dim==2) i2 <- 1:dim(x)[3]
-        if(dim==3) i2 <- 1:dim(x)[2]
+    out <- list()
+    w <- .calcweightmatrix(weight,rel)
+    nd <- which(1:3!=dim)
+    
+    if(dim(w)[4]>1 & dim(w)[4]!=ndata(x)) {
+      #check for possible name collisions in data dimension
+      matches <- sapply(dimnames(w)[[4]],grepl,fulldim(x)[[2]])
+      if(any(colSums(matches)>1)) {
+        similar_subdimensions <- TRUE
+        x <- clean_magpie(x,what = "sets")
+        tmp <- sapply(fulldim(x)[[2]],setequal,dimnames(w)[[4]])
+        subdim2use <- names(tmp)[tmp]
+        if(length(subdim2use)>1) stop("Unclear mapping of weight to data subdimension!")
+        if(length(subdim2use)==0) stop("Could not find data subdimension which matches the given data!")
       } else {
-        i2 <- dimnames(w)[[4]][d]
+        similar_subdimensions <- FALSE      
       }
-      if(dim==1) {
-        if(similar_subdimensions) {
-          selectlist <- list()
-          selectlist[[subdim2use]] <- i2
-          tmp[[d]] <- toolAggregate(mselect(x[,i1,],selectlist),array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=1)
-        } else {
-          tmp[[d]] <- toolAggregate(x[,i1,i2],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=1)
-        }
-      }
-      if(dim==3) tmp[[d]] <- setCells(toolAggregate(x[i1,i2,],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=3),getCells(x[y,1,1])) 
-      if(dim==2) {
-        if(similar_subdimensions) {
-          selectlist <- list()
-          selectlist[[subdim2use]] <- i2
-          tmp[[d]] <- setCells(toolAggregate(mselect(x[i1,,],selectlist),array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=2),getCells(x[y,1,1]))
-        } else {
-          tmp[[d]] <- setCells(toolAggregate(x[i1,,i2],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=2),getCells(x[y,1,1]))
-        }
-      }
+    } else {
+      similar_subdimensions <- FALSE
     }
-    out[[y]] <- mbind(tmp)
-  }  
-  return(setComment(mbind(out),c(comment,paste0("Data aggregated (toolAggregate): ",date()))))
-}
+  
+    for(y in 1:dim(w)[3]) {
+      tmp <- list()
+      if(dim(w)[3]==1) {
+        if(dim==1) i1 <- 1:dim(x)[2]
+        if(dim==2) i1 <- 1:dim(x)[1]
+        if(dim==3) i1 <- 1:dim(x)[1]
+      } else {
+        i1 <- y
+      }
+      for(d in 1:dim(w)[4]) {      
+        if(dim(w)[4]==1) {
+          if(dim==1) i2 <- 1:dim(x)[3]
+          if(dim==2) i2 <- 1:dim(x)[3]
+          if(dim==3) i2 <- 1:dim(x)[2]
+        } else {
+          i2 <- dimnames(w)[[4]][d]
+        }
+        if(dim==1) {
+          if(similar_subdimensions) {
+            selectlist <- list()
+            selectlist[[subdim2use]] <- i2
+            tmp[[d]] <- toolAggregate(mselect(x[,i1,],selectlist),array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=1)
+          } else {
+            tmp[[d]] <- toolAggregate(x[,i1,i2],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=1)
+          }
+        }
+        if(dim==3) tmp[[d]] <- setCells(toolAggregate(x[i1,i2,],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=3),getCells(x[y,1,1])) 
+        if(dim==2) {
+          if(similar_subdimensions) {
+            selectlist <- list()
+            selectlist[[subdim2use]] <- i2
+            tmp[[d]] <- setCells(toolAggregate(mselect(x[i1,,],selectlist),array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=2),getCells(x[y,1,1]))
+          } else {
+            tmp[[d]] <- setCells(toolAggregate(x[i1,,i2],array(w[,,y,d],dim=dim(w)[1:2],dimnames=dimnames(w)[1:2]), dim=2),getCells(x[y,1,1]))
+          }
+        }
+      }
+      out[[y]] <- mbind(tmp)
+    }  
+    return(setComment(mbind(out),c(comment,paste0("Data aggregated (toolAggregate): ",date()))))
+  }
 
   .expand_rel_weight <- function(rel,names,weight,dim){
-  #Expand rel matrix to full dimension if rel is only provided for a subdimension
-  
-  if(round(dim)==dim | suppressWarnings(all(colnames(rel)==names))) {
-    #return rel if nothing has to be done
-    return(list(rel=rel,weight=weight))
-  } 
+    #Expand rel matrix to full dimension if rel is only provided for a subdimension
     
-  if(dim<3) stop("Subdimensions of spatial or temporal dimension are currently not supported!")
+    if(round(dim)==dim | suppressWarnings(all(colnames(rel)==names))) {
+      #return rel if nothing has to be done
+      return(list(rel=rel,weight=weight))
+    } 
+      
+    if(dim<3) stop("Subdimensions of spatial or temporal dimension are currently not supported!")
+    
+    subdim <- round((dim-floor(dim))*10)
+    maxdim <- nchar(gsub("[^\\.]","",names[1])) + 1
   
-  subdim <- round((dim-floor(dim))*10)
-  maxdim <- nchar(gsub("[^\\.]","",names[1])) + 1
-
-  search <- paste0("^(",paste(rep("[^\\.]*\\.",subdim-1),collapse=""),")([^\\.]*)(",paste(rep("\\.[^\\.]*",maxdim-subdim),collapse=""),")$")
-  onlynames <- unique(sub(search,"\\2",names))
-  
-  if(length(setdiff(colnames(rel),onlynames))>0) stop("The provided mapping contains entries which could not be found in the data: ",paste(setdiff(colnames(rel),onlynames),collapse=", "))
-  if(length(setdiff(onlynames,colnames(rel)))>0) stop("The provided data set contains entries not covered by the given mapping: ",paste(setdiff(onlynames,colnames(rel)),collapse=", "))
-  
-  tmp <- unique(sub(search,"\\1#|TBR|#\\3",names)) 
-  additions <- strsplit(tmp,split="#|TBR|#",fixed=TRUE)
-  cnames <- NULL
-  rnames <- NULL
-  for(i in 1:length(additions)) {
-    if(is.na(additions[[i]][2])) additions[[i]][2] <- ""
-    cnames <- c(cnames,paste0(additions[[i]][1],colnames(rel),additions[[i]][2]))
-    rnames <- c(rnames,paste0(additions[[i]][1],rownames(rel),additions[[i]][2]))
+    search <- paste0("^(",paste(rep("[^\\.]*\\.",subdim-1),collapse=""),")([^\\.]*)(",paste(rep("\\.[^\\.]*",maxdim-subdim),collapse=""),")$")
+    onlynames <- unique(sub(search,"\\2",names))
+    
+    if(length(setdiff(colnames(rel),onlynames))>0) stop("The provided mapping contains entries which could not be found in the data: ",paste(setdiff(colnames(rel),onlynames),collapse=", "))
+    if(length(setdiff(onlynames,colnames(rel)))>0) stop("The provided data set contains entries not covered by the given mapping: ",paste(setdiff(onlynames,colnames(rel)),collapse=", "))
+    
+    tmp <- unique(sub(search,"\\1#|TBR|#\\3",names)) 
+    additions <- strsplit(tmp,split="#|TBR|#",fixed=TRUE)
+    cnames <- NULL
+    rnames <- NULL
+    for(i in 1:length(additions)) {
+      if(is.na(additions[[i]][2])) additions[[i]][2] <- ""
+      cnames <- c(cnames,paste0(additions[[i]][1],colnames(rel),additions[[i]][2]))
+      rnames <- c(rnames,paste0(additions[[i]][1],rownames(rel),additions[[i]][2]))
+    }
+    
+    new_rel <- matrix(0,nrow=length(rnames),ncol=length(cnames),dimnames=list(rnames,cnames))
+    
+    for(i in 1:length(additions)) {
+      new_rel[1:nrow(rel)+(i-1)*nrow(rel),1:ncol(rel)+(i-1)*ncol(rel)] <- rel
+    }
+    
+    #reorder and possibly filter entries in new_rel so that it fits names
+    new_rel <- new_rel[,names]
+    
+    if(is.null(weight)) return(list(rel=new_rel,weight=weight))
+    
+    if(ncol(new_rel)>=nrow(new_rel)) {
+      requirednames <- colnames(new_rel)  
+    } else {
+      requirednames <- rownames(new_rel)
+    }
+    
+    #weight is already related to full dimensionality
+    if(all(requirednames %in% getNames(weight))) return(list(rel=new_rel,weight=weight))
+    
+    new_weight <- weight[,,rep(1,length(requirednames))]
+    getNames(new_weight) <- requirednames
+    for(n in getNames(weight)) new_weight[,,n] <- weight[,,n]
+    
+    return(list(rel=new_rel,weight=new_weight))  
   }
-  
-  new_rel <- matrix(0,nrow=length(rnames),ncol=length(cnames),dimnames=list(rnames,cnames))
-  
-  for(i in 1:length(additions)) {
-    new_rel[1:nrow(rel)+(i-1)*nrow(rel),1:ncol(rel)+(i-1)*ncol(rel)] <- rel
-  }
-  
-  #reorder and possibly filter entries in new_rel so that it fits names
-  new_rel <- new_rel[,names]
-  
-  if(is.null(weight)) return(list(rel=new_rel,weight=weight))
-  
-  if(ncol(new_rel)>=nrow(new_rel)) {
-    requirednames <- colnames(new_rel)  
-  } else {
-    requirednames <- rownames(new_rel)
-  }
-  
-  #weight is already related to full dimensionality
-  if(all(requirednames %in% getNames(weight))) return(list(rel=new_rel,weight=weight))
-  
-  new_weight <- weight[,,rep(1,length(requirednames))]
-  getNames(new_weight) <- requirednames
-  for(n in getNames(weight)) new_weight[,,n] <- weight[,,n]
-  
-  return(list(rel=new_rel,weight=new_weight))  
-}
 
   if(!is.magpie(x)) {
     stop("Input is not a MAgPIE object, x has to be a MAgPIE object!")
