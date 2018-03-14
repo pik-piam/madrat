2057124d4167851469deb96571afb38c6aba8ea4
Stephen Bi <stephenb@pik-potsdam.de>
2018-03-14 16:35:43 +0100

    Merge with implementation of metadata handling

--- R/calcOutput.R
+++ R/calcOutput.R
@@ -60,7 +60,7 @@
 #' }
 #' 
 #' @importFrom magclass nyears nregions getComment<- getComment getYears clean_magpie write.report2 write.magpie
-#' getCells getYears<- is.magpie dimSums
+#' getCells getYears<- is.magpie dimSums getMetadata updateMetadata
 #' @importFrom utils packageDescription read.csv2
 #' @export
 
@@ -68,6 +68,7 @@ calcOutput <- function(type,aggregate=TRUE,file=NULL,years=NULL,round=NULL, dest
  
   cwd <- getwd()
   if(is.null(getOption("gdt_nestinglevel"))) vcat(1,"")
+  options(reducedHistory=TRUE)
   startinfo <- toolstartmessage("+")
   if(!file.exists(getConfig("outputfolder"))) dir.create(getConfig("outputfolder"),recursive = TRUE)
   setwd(getConfig("outputfolder"))
@@ -150,8 +151,7 @@ calcOutput <- function(type,aggregate=TRUE,file=NULL,years=NULL,round=NULL, dest
   if(!is.null(x$max)) {
     if(any(x$x>x$max, na.rm = TRUE)) vcat(0,"Data returned by ", functionname," contains values greater than the predefined maximum (max = ",x$max,")")
   }
-  if(na_warning) if(anyNA(x$x)) vcat(0,"Data returned by ", functionname," contains NAs")
-  if(any(is.infinite(x$x))) vcat(0,"Data returned by ", functionname," contains infinite values")
+  if(anyNA(x$x) & na_warning) vcat(0,"Data returned by ", functionname," contains NAs")
   
   if(!is.null(years)){
     #check that years exist in provided data
@@ -175,14 +175,11 @@ calcOutput <- function(type,aggregate=TRUE,file=NULL,years=NULL,round=NULL, dest
     return(x)
   }
   
-  
-  unit <- .prep_comment(x$unit,"unit",paste0('Missing unit information for data set "',type,'"!'))
-  description <- .prep_comment(x$description,"description",paste0('Missing description for data set "',type,'"! Please add a description in the corresponding calc function!'))
   comment <- .prep_comment(getComment(x$x),"comment")
-  note <- .prep_comment(x$note,"note")
   origin <- .prep_comment(paste0(gsub("\\s{2,}"," ",paste(deparse(match.call()),collapse=""))," (madrat ",packageDescription("madrat")$Version," | ",x$package,")"),"origin")
   date <- .prep_comment(date(),"creation date")
   
+  #Mx <- getMetadata(x$x)
   if(aggregate==TRUE) {
     x$x <- toolAggregate(x$x,toolMappingFile("regional",getConfig("regionmapping")),weight=x$weight, mixed_aggregation=x$mixed_aggregation)
   } else if (toupper(aggregate)=="GLO") {
@@ -201,15 +198,10 @@ calcOutput <- function(type,aggregate=TRUE,file=NULL,years=NULL,round=NULL, dest
     x$x <- round(x$x,round)
   }
 
-  getComment(x$x) <- c(description,
-                     unit,
-                     note,
-                     comment,
-                     origin,
-                     date)  
+  getComment(x$x) <- c(comment,origin,date)  
   
-  x$x<-clean_magpie(x$x)
-  
+  x$x<-updateMetadata(clean_magpie(x$x),unit=x$unit,source=x$source,calcHistory="update",description=x$description)
+
   if(is.null(file) & append){
     vcat(0,"The parameter append=TRUE works only when the file name is provided in the calcOutput() function call.")
   }
@@ -223,6 +215,8 @@ calcOutput <- function(type,aggregate=TRUE,file=NULL,years=NULL,round=NULL, dest
     }
     if(!is.null(destination)) file2destination(file=file,destination=destination)
   }
+  
+  if (length(sys.calls())==1)  options(reducedHistory = FALSE)
   
   if(supplementary) {
     return(x)
