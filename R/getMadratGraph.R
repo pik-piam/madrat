659d4bbe165f59262739d41b6c03952c0aded758
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-04-21 16:11:37 +0200

    added missing export statement

--- R/getMadratGraph.R
+++ R/getMadratGraph.R
@@ -11,6 +11,7 @@
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{getCalculations}}, \code{\link{getConfig}}
 #' @importFrom stringi stri_match_all_regex
+#' @export
 
 getMadratGraph <- function(packages=getConfig("packages"), globalenv=getConfig("globalenv")) {
   
