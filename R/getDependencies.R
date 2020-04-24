893fe34362d1e863dfd43e46eb9d79e815ea6458
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-04-24 10:36:30 +0200

    added package information to getDependencies

--- R/getDependencies.R
+++ R/getDependencies.R
@@ -5,9 +5,12 @@
 #' 
 #' 
 #' @param name name of the function to be analyzed
-#' @param direction Character string, either “in”, “out” or “all”. If “in” 
+#' @param direction Character string, either “in”, “out”, "both" or “full”. If “in” 
 #' all sources feeding into the function are listed. If “out” consumer of the 
-#' function are listed. If “all” returns the union of these.
+#' function are listed. If “both” the union of "in" and "out" is returned.
+#' If "full" the full network this function is connected to is shown, including
+#' indirect connections to functions which neither source nor consume the given
+#' function but serve as sources to other consumer functions.
 #' @param graph A madrat graph as returned by \code{\link{getMadratGraph}}. 
 #' Will be created with \code{\link{getMadratGraph}} if not provided. 
 #' @param ... Additional arguments for \code{\link{getMadratGraph}} in case
@@ -19,8 +22,22 @@
 getDependencies <- function(name, direction="in", graph=NULL, ...) {
   if (!requireNamespace("igraph", quietly = TRUE)) stop("Package \"igraph\" needed for this function to work.")
   if(is.null(graph)) graph <- getMadratGraph(...)
-  tmp <- igraph::subcomponent(igraph::graph_from_data_frame(graph),name,direction)
-  tmp <- attr(tmp,"names")
-  return(setdiff(tmp,name))
+  if(!(name %in% c(graph$from,graph$to))) stop("There is no function with the name \"",name,"\"")
+  ggraph <- igraph::graph_from_data_frame(graph)
+  if(direction=="full") direction <- "all"
+  if(direction=="both") {
+    tmp <- unique(c(attr(igraph::subcomponent(ggraph,name,"in"),"names"),
+                    attr(igraph::subcomponent(ggraph,name,"out"),"names")))
+  } else {
+    tmp <- attr(igraph::subcomponent(ggraph,name,direction),"names")
+  }
+  tmp <- setdiff(tmp,name)
+  
+  packages <- c(graph$from_package,graph$to_package)
+  names(packages) <- c(graph$from,graph$to)
+  
+  out <- data.frame(func=tmp,package=packages[tmp],row.names=NULL)
+  
+  return(data.frame(out[order(out$package),],row.names=NULL))
 }
 
