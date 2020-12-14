f2e75aa9f2e5bc6d59ffa9179f938bb433a97c09
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2020-12-14 16:08:10 +0100

    add tests

--- tests/testthat/test-toolGetMapping.R
+++ tests/testthat/test-toolGetMapping.R
@@ -6,8 +6,12 @@ test_that("toolGetMapping works", {
                              CountryCode = c("ABW", "AFG", "AGO","AIA", "ALA", "ALB"), 
                              RegionCode = c("LAM", "OAS", "SSA", "LAM","EUR", "NEU")), 
                         row.names = c(NA, 6L), class = "data.frame")
+  write.csv(expected,file = "expected.csv",quote = FALSE,row.names = FALSE)
   expect_identical(head(toolGetMapping("regionmappingH12.csv", where="madrat")), expected)
   expect_identical(toolGetMapping("bla",where="local", returnPathOnly = TRUE), "bla")
   expect_identical(toolGetMapping("/bla",where="local", returnPathOnly = TRUE), "/bla")
   expect_identical(toolGetMapping("/bla",type=".",where="local", returnPathOnly = TRUE), "./bla")
+  expect_identical(toolGetMapping("expected.csv"), expected)
+  unlink("expected.csv",force = TRUE)
+  expect_identical(head(toolGetMapping("regionmappingH12.csv")), expected)
 })
