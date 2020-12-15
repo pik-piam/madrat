7e1beace364c8b2acb45a8dd899671330fef1eb9
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2020-12-15 20:29:55 +0100

    improve tests of toolGetMapping, check if the routine can find an unknown mapping only by name. no files created/changed/deleted

--- tests/testthat/test-toolGetMapping.R
+++ tests/testthat/test-toolGetMapping.R
@@ -6,12 +6,16 @@ test_that("toolGetMapping works", {
                              CountryCode = c("ABW", "AFG", "AGO","AIA", "ALA", "ALB"), 
                              RegionCode = c("LAM", "OAS", "SSA", "LAM","EUR", "NEU")), 
                         row.names = c(NA, 6L), class = "data.frame")
-  write.csv(expected,file = "expected.csv",quote = FALSE,row.names = FALSE)
+  # find a mapping file in the current package that is not found also 
+  # in the obvious place getConfig(mappingfolder)
+  fname <- setdiff(grep("csv",dir(paste0(system.file(package = "madrat"),"/extdata")),value=TRUE),
+                   grep("csv",dir(as.character(getConfig("mappingfolder"))),value=TRUE))[1]
+  full_path <- paste0(system.file(package = "madrat"),"/extdata/",fname)
+  # try to find that mapping as if you did not know where it is
+  expect_identical(toolGetMapping(fname), read.csv(full_path,header = TRUE,sep = ";",colClasses = "character"))
   expect_identical(head(toolGetMapping("regionmappingH12.csv", where="madrat")), expected)
   expect_identical(toolGetMapping("bla",where="local", returnPathOnly = TRUE), "bla")
   expect_identical(toolGetMapping("/bla",where="local", returnPathOnly = TRUE), "/bla")
   expect_identical(toolGetMapping("/bla",type=".",where="local", returnPathOnly = TRUE), "./bla")
-  expect_identical(toolGetMapping("expected.csv"), expected)
-  unlink("expected.csv",force = TRUE)
   expect_identical(head(toolGetMapping("regionmappingH12.csv")), expected)
 })
