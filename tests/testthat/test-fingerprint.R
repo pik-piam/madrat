08f233e00e5669b251db6339c6a5c9ab6d29324f
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-03-29 14:49:51 +0200

    added tests

--- tests/testthat/test-fingerprint.R
+++ tests/testthat/test-fingerprint.R
@@ -46,5 +46,50 @@ test_that("fingerprinting works for edge cases", {
   expect_identical(attr(fp,"details")[-1], c(readFingerprintTest="b5efba0b","map.csv"="7e76cec7"))
 })
 
+test_that("fingerprinting works with control flags", {
+  setConfig(globalenv = TRUE, .verbose = FALSE, mainfolder = tempdir(), verbosity = 1)
+  setConfig(globalenv = TRUE)
+  readData <- function() return(1)
+  readData2 <- function() return(2)
+  calcExample2 <- function() {
+    a <- readSource("Data")
+    if(FALSE) b <- readSource("Data2")
+  }
+  globalassign("readData","readData2","calcExample2")
+  fp_expected <- structure("7e60a810", details = c(calcExample2 = "8386790d", 
+                                                   readData = "783a5e2f", 
+                                                   readData2 = "fb52578f"))
+  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"),fp_expected)
+  readData3 <- function()return(3)
+  calcExample2 <- function() {
+    "!# @monitor readData3"
+    "!# @ignore  readData2"
+    a <- readSource("Data")
+    if(FALSE) b <- readSource("Data2")
+  }
+  calcExample3 <- function() {
+    a <- calcOutput("Example2", aggregate=FALSE)
+  }
+  calcExample4 <- function() {
+    "!# @monitor readData2"
+    "!# @ignore  readData2"
+  }
+  globalassign("readData3","calcExample2","calcExample3","calcExample4")
+  
+  fp2_expected <- structure("7b150c88", details = c(calcExample2 = "ded5ab1f", 
+                                    readData = "783a5e2f", readData3 = "2cd95ca8"))
+  fp3_expected <- structure("edf89c03", details = c(calcExample2 = "ded5ab1f", 
+                                                    calcExample3 = "94819ad1", 
+                                                    readData = "783a5e2f", 
+                                                    readData2 = "fb52578f", 
+                                                    readData3 = "2cd95ca8"))
+  fp4_expected <- structure("91bbc100", details = c(calcExample4 = "9aad0909", 
+                                                    readData2 = "fb52578f"))
+  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"), fp2_expected)
+  expect_identical(madrat:::fingerprint("calcExample3", details = TRUE, packages = "madrat"), fp3_expected)
+  expect_identical(madrat:::fingerprint("calcExample4", details = TRUE, packages = "madrat"), fp4_expected)
+  
+})
+
 
 
