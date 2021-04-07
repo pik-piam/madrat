2f304ff5bc9956f64054b264d085b455eb35dcda
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-04-07 15:42:02 +0200

    added tests

---
+++ tests/testthat/test-getISOlist.R
@@ -0,0 +1,14 @@
+context("getISOlist")
+
+test_that("getISOlist works", {
+ expect_equal(length(getISOlist()), 249)
+ expect_mapequal(c(getISOlist("important"), getISOlist("dispensable")), getISOlist())
+})
+
+test_that("data sets used in getISOlist are consistent to each other", {
+  expect_silent(ref <- getISOlist())
+  pop2015 <- read.magpie(system.file("extdata","pop2015.csv",package = "madrat"))
+  expect_equal(length(ref),ncells(pop2015))
+  expect_equal(unname(ref),getCells(pop2015))
+  expect_error(getISOlist("blub"), "Unknown type")
+})
