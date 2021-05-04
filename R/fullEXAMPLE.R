9e1ca3186b86ce002178284517193c58d9cce213
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-05-04 15:07:37 +0200

    type 2 upgrade

--- R/fullEXAMPLE.R
+++ R/fullEXAMPLE.R
@@ -28,6 +28,6 @@ fullEXAMPLE <- function(rev = 0, dev = "") {
   if (dev == "test") {
     message("Here you could execute code for a hypothetical development version called \"test\"")
   }
-  # return is not required, but can be used to append a tag to the resulting file
+  # return is not required, but can be used to append a tag to the resulting filename
   return(list(tag = "customizable_tag"))
 }
