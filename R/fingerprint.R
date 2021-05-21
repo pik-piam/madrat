c85d759f3ab2e7043316f6d8d73a6fc6e74bc977
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-05-21 12:03:42 +0200

    updated fingerprintFiles function (hash cache + option to ignore folder)

--- R/fingerprint.R
+++ R/fingerprint.R
@@ -1,5 +1,5 @@
 #' Tool: fingerprint
-#' 
+#'
 #' Function which creates a unique fingerprint for a madrat function based on
 #' the code of the function itself, other madrat functions which are called by
 #' this function and of all source folders involved in the process.
@@ -9,7 +9,7 @@
 #' will stay the same, otherwise it will change. Hence, it can be to figure out
 #' whether a cache file can be used for further calculations, or whether the
 #' calculation should be redone.
-#' 
+#'
 #' @note For a better performance only the first 300 bytes of each file and the
 #' corresponding file size is hashed.
 #' As the fingerprint function only takes madrat-based functions into account
@@ -18,7 +18,7 @@
 #' is still the same. In a similar fashion it is possible that the fingerprint
 #' changes even though the workflow stayed the same
 #' (as the dependencies are sometimes overestimated).
-#' 
+#'
 #' @param name Name of the function to be analyzed
 #' @param details Boolean indicating whether additional details in form
 #' of an attribute with underlying hash information should be added or not
@@ -30,48 +30,51 @@
 #' @author Jan Philipp Dietrich, Pascal Führlich
 #' @seealso \code{\link{readSource}}
 #' @examples
-#' madrat:::fingerprint("toolGetMapping", package="madrat")
+#' madrat:::fingerprint("toolGetMapping", package = "madrat")
 #' @importFrom digest digest
 
-fingerprint <- function(name, details=FALSE, graph = NULL, ...) {
+fingerprint <- function(name, details = FALSE, graph = NULL, ...) {
   dependencies <- getDependencies(name, direction = "in", self = TRUE, graph = graph, ...)
   result <- tryCatch({
+      fingerprintFunctions <- dependencies$hash[robustOrder(dependencies$call)]
+      names(fingerprintFunctions) <- dependencies$call[robustOrder(dependencies$call)]
 
-    fingerprintFunctions <- dependencies$hash[robustOrder(dependencies$call)]
-    names(fingerprintFunctions) <- dependencies$call[robustOrder(dependencies$call)]
-
-    # handle special requests via flags
-    .tmp <- function(x) return(robustSort(sub(":+", ":::", x)))
-    ignore  <- .tmp(attr(dependencies, "flags")$ignore)
-    monitor <- .tmp(attr(dependencies, "flags")$monitor)
-    # if conflicting information is giving (monitor and ignore at the same time,
-    # prioritize monitor request)
-    ignore <- setdiff(ignore, monitor)
-    # add calls from the monitor list which are not already monitored
-    fingerprintMonitored <- fingerprintCall(setdiff(monitor, names(fingerprintFunctions)))
-    # ignore functions mentioned in the ignore list
-    fingerprintFunctions <- fingerprintFunctions[setdiff(names(fingerprintFunctions), ignore)]
-    sources <- substring(dependencies$func[dependencies$type == "read"], 5)
-    if (length(sources) > 0) {
-      sources <- paste0(getConfig("sourcefolder"), "/", robustSort(sources))
-    }
-    fingerprintSources <- fingerprintFiles(sources)
-    fingerprintMappings <- fingerprintFiles(attr(dependencies, "mappings"))
-    fingerprint <- c(fingerprintFunctions, fingerprintSources, fingerprintMappings, fingerprintMonitored)
-    fingerprint <- fingerprint[robustOrder(basename(names(fingerprint)))]
-    out <- digest(unname(fingerprint), algo = getConfig("hash"))
-    if (details) {
-      attr(out, "details") <- fingerprint
-      vcat(3, "hash components (", out, "):", show_prefix = FALSE)
-      for (n in names(fingerprint)) {
-        vcat(3, "  ", fingerprint[n], " | ", basename(n), " | ", n, show_prefix = FALSE)
+      # handle special requests via flags
+      .tmp <- function(x) {
+        return(robustSort(sub(":+", ":::", x)))
       }
+      ignore <- .tmp(attr(dependencies, "flags")$ignore)
+      monitor <- .tmp(attr(dependencies, "flags")$monitor)
+      # if conflicting information is giving (monitor and ignore at the same time,
+      # prioritize monitor request)
+      ignore <- setdiff(ignore, monitor)
+      # add calls from the monitor list which are not already monitored
+      fingerprintMonitored <- fingerprintCall(setdiff(monitor, names(fingerprintFunctions)))
+      # ignore functions mentioned in the ignore list
+      fingerprintFunctions <- fingerprintFunctions[setdiff(names(fingerprintFunctions), ignore)]
+      sources <- substring(dependencies$func[dependencies$type == "read"], 5)
+      if (length(sources) > 0) {
+        sources <- paste0(getConfig("sourcefolder"), "/", robustSort(sources))
+      }
+      fingerprintSources <- fingerprintFiles(sources)
+      fingerprintMappings <- fingerprintFiles(attr(dependencies, "mappings"))
+      fingerprint <- c(fingerprintFunctions, fingerprintSources, fingerprintMappings, fingerprintMonitored)
+      fingerprint <- fingerprint[robustOrder(basename(names(fingerprint)))]
+      out <- digest(unname(fingerprint), algo = getConfig("hash"))
+      if (details) {
+        attr(out, "details") <- fingerprint
+        vcat(3, "hash components (", out, "):", show_prefix = FALSE)
+        for (n in names(fingerprint)) {
+          vcat(3, "  ", fingerprint[n], " | ", basename(n), " | ", n, show_prefix = FALSE)
+        }
+      }
+      out
+    },
+    error = function(error) {
+      vcat(2, paste(" - Fingerprinting failed:", error), show_prefix = FALSE)
+      return("fingerprintError")
     }
-    out
-  }, error = function(error) {
-    vcat(2, paste(" - Fingerprinting failed:", error), show_prefix = FALSE)
-    return("fingerprintError")
-  })
+  )
   attr(result, "call") <- dependencies$call[dependencies$func == name]
   return(result)
 }
@@ -79,38 +82,50 @@ fingerprint <- function(name, details=FALSE, graph = NULL, ...) {
 fingerprintCall <- function(name) {
   .tmp <- function(x) {
     f <- try(eval(parse(text = x)), silent = TRUE)
-    if ("try-error" %in% class(f)) return(NULL)
+    if ("try-error" %in% class(f)) {
+      return(NULL)
+    }
     return(digest(paste(deparse(f), collapse = " "), algo = getConfig("hash")))
   }
   return(unlist(sapply(name, .tmp)))
 }
 
 fingerprintFiles <- function(paths) {
-  if (length(paths) == 0) return(NULL)
+  if (length(paths) == 0) {
+    return(NULL)
+  }
   paths <- paths[file.exists(paths)]
-  if (length(paths) == 0) return(NULL)
+  if (length(paths) == 0) {
+    return(NULL)
+  }
   .tmp <- function(path) {
-    if (file.exists(paste0(path,"/DONTFINGERPRINT"))) return("SKIPPED")
+    if (file.exists(paste0(path, "/DONTFINGERPRINT"))) {
+      return("SKIPPED")
+    }
     if (dir.exists(path)) {
-      files <- data.frame(name = robustSort(list.files(path, recursive = TRUE, full.names = FALSE)),
-                          stringsAsFactors = FALSE)
-      files <- files[files$name != "madratHashCache.rds",,drop = FALSE]
+      files <- data.frame(
+        name = robustSort(list.files(path, recursive = TRUE, full.names = FALSE)),
+        stringsAsFactors = FALSE
+      )
+      files <- files[files$name != "madratHashCache.rds", , drop = FALSE]
     } else {
       files <- data.frame(name = basename(path), stringsAsFactors = FALSE)
-      path  <- dirname(path)
+      path <- dirname(path)
     }
     if (nrow(files) == 0) {
       files <- NULL
     } else {
-      files$path  <- paste0(path,"/",files$name)
+      files$path <- paste0(path, "/", files$name)
       files$mtime <- file.mtime(files$path)
-      files$size  <- file.size(files$path)
+      files$size <- file.size(files$path)
       # create key to identify entries which require recalculation
-      files$key   <- apply(files[c("name", "mtime", "size")], 1, 
-                           digest, algo = getConfig("hash"))
+      files$key <- apply(files[c("name", "mtime", "size")], 1,
+        digest,
+        algo = getConfig("hash")
+      )
     }
-    
-    hashCacheFile <- paste0(path,"/madratHashCache.rds")
+
+    hashCacheFile <- paste0(path, "/madratHashCache.rds")
     if (file.exists(hashCacheFile)) {
       filesCache <- readRDS(hashCacheFile)
       # keep only entries which are still up-to-date
@@ -121,21 +136,21 @@ fingerprintFiles <- function(paths) {
     } else {
       filesCache <- NULL
     }
-    
+
     if (!is.null(files)) {
       # use the first 300 byte of each file and the file sizes for hashing
-      files$hash  <- sapply(files$path, digest, algo = getConfig("hash"), file = TRUE, length = 300)
-      files$path  <- NULL
+      files$hash <- sapply(files$path, digest, algo = getConfig("hash"), file = TRUE, length = 300)
+      files$path <- NULL
       # write cachefile if path belongs to the source folder
-      if (dir.exists(getConfig("sourcefolder")) && 
-          startsWith(normalizePath(path), normalizePath(getConfig("sourcefolder")))) {
-        saveRDS(files, paste0(path,"/madratHashCache.rds"), version = 2)
+      if (dir.exists(getConfig("sourcefolder")) &&
+        startsWith(normalizePath(path), normalizePath(getConfig("sourcefolder")))) {
+        saveRDS(files, paste0(path, "/madratHashCache.rds"), version = 2)
       }
     }
     files <- rbind(filesCache, files)
     files$mtime <- NULL
-    files$key   <- NULL
-    return(digest(files[robustOrder(files$name),], algo = getConfig("hash")))
+    files$key <- NULL
+    return(digest(files[robustOrder(files$name), ], algo = getConfig("hash")))
   }
   return(sapply(paths, .tmp))
 }
--- tests/testthat/test-fingerprint.R
+++ tests/testthat/test-fingerprint.R
@@ -1,7 +1,7 @@
 context("fingerprinting")
 
 globalassign <- function(...) {
-  for (x in c(...)) assign(x,eval.parent(parse(text = x)),.GlobalEnv)
+  for (x in c(...)) assign(x, eval.parent(parse(text = x)), .GlobalEnv)
 }
 
 test_that("fingerprinting works as expected", {
@@ -14,7 +14,7 @@ test_that("fingerprinting works as expected", {
   }
   globalassign("toolTest")
   expect_equivalent(madrat:::fingerprint("toolTest", packages = "madrat", globalenv = TRUE), "8b3413cc")
-  emptyfolder <- paste0(tempdir(),"/empty")
+  emptyfolder <- paste0(tempdir(), "/empty")
   dir.create(emptyfolder, recursive = TRUE, showWarnings = FALSE)
   expect_equal(unname(madrat:::fingerprintFiles(emptyfolder)), "bc4159c0")
   unlink(emptyfolder)
@@ -25,7 +25,7 @@ test_that("fingerprintFiles works as expected", {
   cwd <- getwd()
   setwd(tempdir())
   on.exit(setwd(cwd))
-  writeLines("this is a test","test.txt")
+  writeLines("this is a test", "test.txt")
   fp <- madrat:::fingerprintFiles("test.txt")
   expect_identical(fp, c(test.txt = "94877d66"))
 })
@@ -35,13 +35,15 @@ test_that("fingerprinting works for edge cases", {
   cwd <- getwd()
   setwd(tempdir())
   on.exit(setwd(cwd))
-  writeLines("this is a test","map.csv")
+  writeLines("this is a test", "map.csv")
   readFingerprintTest <- function() {
     map <- toolGetMapping("map.csv", where = "local")
     return(1)
   }
   globalassign("readFingerprintTest")
-  expect_silent({fp <- madrat:::fingerprint("readFingerprintTest", packages = getConfig("packages"), details = TRUE)})
+  expect_silent({
+    fp <- madrat:::fingerprint("readFingerprintTest", packages = getConfig("packages"), details = TRUE)
+  })
   expect_identical(attr(fp, "details")[-1], c("map.csv" = "12128ff8", readFingerprintTest = "b5efba0b"))
   expect_null(madrat:::fingerprintCall("blub"))
 })
@@ -49,51 +51,70 @@ test_that("fingerprinting works for edge cases", {
 test_that("fingerprinting works with control flags", {
   setConfig(globalenv = TRUE, .verbose = FALSE, mainfolder = tempdir(), verbosity = 1)
   setConfig(globalenv = TRUE)
-  readData <- function() return(1)
-  readData2 <- function() return(2)
+  readData <- function() {
+    return(1)
+  }
+  readData2 <- function() {
+    return(2)
+  }
   calcExample2 <- function() {
     a <- readSource("Data")
-    if(FALSE) b <- readSource("Data2")
+    if (FALSE) b <- readSource("Data2")
   }
-  globalassign("readData","readData2","calcExample2")
-  fp_expected <- structure("7e60a810", call = "calcExample2", 
-                                       details = c(calcExample2 = "8386790d", 
-                                                   readData = "783a5e2f", 
-                                                   readData2 = "fb52578f"))
-  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"),fp_expected)
-  readData3 <- function()return(3)
+  globalassign("readData", "readData2", "calcExample2")
+  fpExpected <- structure("a18f3e07",
+    call = "calcExample2",
+    details = c(
+      calcExample2 = "8386790d",
+      readData = "667d53aa",
+      readData2 = "041bef7f"
+    )
+  )
+  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"), fpExpected)
+  readData3 <- function() {
+    return(3)
+  }
   calcExample2 <- function() {
     "!# @monitor readData3"
     "!# @ignore  readData2"
     a <- readSource("Data")
-    if(FALSE) b <- readSource("Data2")
+    if (FALSE) b <- readSource("Data2")
   }
   calcExample3 <- function() {
-    a <- calcOutput("Example2", aggregate=FALSE)
+    a <- calcOutput("Example2", aggregate = FALSE)
   }
   calcExample4 <- function() {
     "!# @monitor readData2"
     "!# @ignore  readData2"
   }
-  globalassign("readData3","calcExample2","calcExample3","calcExample4")
-  
-  fp2_expected <- structure("7b150c88", call = "calcExample2",
-                                        details = c(calcExample2 = "ded5ab1f", 
-                                    readData = "783a5e2f", readData3 = "2cd95ca8"))
-  fp3_expected <- structure("edf89c03", call = "calcExample3",
-                                        details = c(calcExample2 = "ded5ab1f", 
-                                                    calcExample3 = "94819ad1", 
-                                                    readData = "783a5e2f", 
-                                                    readData2 = "fb52578f", 
-                                                    readData3 = "2cd95ca8"))
-  fp4_expected <- structure("91bbc100", call = "calcExample4",
-                                        details = c(calcExample4 = "9aad0909", 
-                                                    readData2 = "fb52578f"))
-  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"), fp2_expected)
-  expect_identical(madrat:::fingerprint("calcExample3", details = TRUE, packages = "madrat"), fp3_expected)
-  expect_identical(madrat:::fingerprint("calcExample4", details = TRUE, packages = "madrat"), fp4_expected)
-  rm(list = c("readData3","calcExample2","calcExample3","calcExample4"), envir = .GlobalEnv)
+  globalassign("readData3", "calcExample2", "calcExample3", "calcExample4")
+
+  fp2Expected <- structure("4b34fe34",
+    call = "calcExample2",
+    details = c(
+      calcExample2 = "ded5ab1f",
+      readData = "667d53aa", readData3 = "56324bcf"
+    )
+  )
+  fp3Expected <- structure("a2236072",
+    call = "calcExample3",
+    details = c(
+      calcExample2 = "ded5ab1f",
+      calcExample3 = "94819ad1",
+      readData = "667d53aa",
+      readData2 = "041bef7f",
+      readData3 = "56324bcf"
+    )
+  )
+  fp4Expected <- structure("1def7910",
+    call = "calcExample4",
+    details = c(
+      calcExample4 = "9aad0909",
+      readData2 = "041bef7f"
+    )
+  )
+  expect_identical(madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat"), fp2Expected)
+  expect_identical(madrat:::fingerprint("calcExample3", details = TRUE, packages = "madrat"), fp3Expected)
+  expect_identical(madrat:::fingerprint("calcExample4", details = TRUE, packages = "madrat"), fp4Expected)
+  rm(list = c("readData3", "calcExample2", "calcExample3", "calcExample4"), envir = .GlobalEnv)
 })
-
-
-
