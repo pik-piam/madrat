1456d85fc86859ee04a35fc51296ef5d5e6913d2
Jan Dietrich <dietrich@pik-potsdam.de>
2021-09-20 14:32:05 +0200

    Update readTau.R

--- R/readTau.R
+++ R/readTau.R
@@ -4,7 +4,7 @@
 #' Dietrich J.P., Schmitz C., Mueller C., Fader M., Lotze-Campen H., Popp A.,
 #' Measuring agricultural land-use intensity - A global analysis using a
 #' model-assisted approach, Ecological Modelling, Volume 232, 10 May 2012,
-#' Pages 109-118, ISSN 0304-3800, 10.1016/j.ecolmodel.2012.03.002.#' 
+#' Pages 109-118, ISSN 0304-3800, 10.1016/j.ecolmodel.2012.03.002.
 #' 
 #' @param subtype Type of Tau data that should be read. Available types are:
 #' \itemize{ \item \code{paper}: numbers as they are reported in the paper
