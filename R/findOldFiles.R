efe2c4817044ad0c5f11a4395967a6f4d407261c
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-10-25 17:00:00 +0200

    findOldFiles possible with atime/mtime/ctime

--- R/findOldFiles.R
+++ R/findOldFiles.R
@@ -1,20 +1,22 @@
 #' findOldFiles
 #'
-#' Find files older than the specified number of days, based on atime/last access time file metadata. This
-#' metadata is not always available and it is system dependent what atime/last access time actually means.
+#' Find files older than the specified number of days, based on file time metadata. This metadata is not available on
+#' all systems and the semantics are also system dependent.
 #'
 #' @param daysThreshold Files older than this many days are returned.
 #' @param path Path to where to look for old files.
+#' @param timeType Which file metadata time should be used. One of atime (last access time), mtime (last modify time),
+#' ctime (last metadata change).
 #' @value A data.frame as returned by base::file.info, containing only files older than <daysThreshold> days.
 #' @export
-findOldFiles <- function(daysThreshold, path) {
+findOldFiles <- function(daysThreshold, path, timeType = c("atime", "mtime", "ctime")) {
   stopifnot(length(daysThreshold) == 1, is.numeric(daysThreshold), daysThreshold >= 0,
             length(path) == 1, dir.exists(path))
+  timeType <- match.arg(timeType)
 
-  path <- normalizePath(path, winslash = "/")
-  filesAccessTimes <- file.info(list.files(path, full.names = TRUE))
+  filesAccessTimes <- file.info(list.files(normalizePath(path, winslash = "/"), full.names = TRUE))
 
   dateThreshold <- Sys.time() - daysThreshold * 24 * 60 * 60
-  oldFiles <- filesAccessTimes[filesAccessTimes[["atime"]] < dateThreshold, ]
+  oldFiles <- filesAccessTimes[filesAccessTimes[[timeType]] < dateThreshold, ]
   return(oldFiles)
 }
--- tests/testthat/test-findOldFiles.R
+++ tests/testthat/test-findOldFiles.R
@@ -1,12 +1,15 @@
 test_that("findOldFiles finds old files", {
-  # skip_on_os("windows") # Sys.setFileTime does not modify atime (only mtime/'last write time') on windows
-  # should also skip on systems where atimes are not available
+  # Sys.setFileTime does not modify atime (only mtime/'last write time') on windows
+  timeType <- if (identical(Sys.info()[["sysname"]], "Windows")) "mtime" else "atime"
 
   cacheFolder <- withr::local_tempdir()
   cacheFile <- normalizePath(file.path(cacheFolder, "cacheFile"), winslash = "/", mustWork = FALSE)
   file.create(cacheFile)
 
-  expect_identical(nrow(findOldFiles(30, cacheFolder)), 0L)
+  expect_identical(nrow(findOldFiles(30, cacheFolder, timeType)), 0L)
   Sys.setFileTime(cacheFile, Sys.time() - 31 * 24 * 60 * 60) # set atime to 31 days ago
-  expect_identical(rownames(findOldFiles(30, cacheFolder)), cacheFile)
+  expect_identical(rownames(findOldFiles(30, cacheFolder, timeType)), cacheFile)
+
+  expect_error(findOldFiles(30, cacheFolder, "stime"),
+               "'arg' should be one of \"atime\", \"mtime\", \"ctime\"", fixed = TRUE)
 })
