62e237d10b5271515b662fa80fcc8a9fc0893256
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-01-19 16:00:27 +0100

    added bundleCompile prototype

---
+++ R/bundleCompile.R
@@ -0,0 +1,43 @@
+#' bundleCompile
+#'
+#' Function which takes a bundle tgz-file as create via \code{\link{retrieveData}}
+#' and compiles the corresponding output with the given settings (in most cases
+#' with a given region mapping)
+#'
+#'
+#' @param bundle path to a bundle file
+#' @param regionmapping region mapping to be used for aggregation. If not 
+#' specified the current default will be used.
+#' @param ... (Optional) Settings that should be changed in addition. NOTE:
+#' which settings can be modified varies from bundle to bundle and unsupported
+#' choices can lead to errors.
+#' @author Jan Philipp Dietrich
+#' @seealso
+#' \code{\link{retrieveData}},\code{\link{setConfig}}
+#' @examples
+#' \dontrun{
+#' bundleCompile("rev1_bundle_example.tgz", regionmapping = "regionmappingH12.csv")
+#' }
+#' @importFrom withr with_tempdir
+#' @importFrom utils untar
+#' @importFrom yaml read_yaml
+#' @export
+bundleCompile <- function(bundle, regionmapping = getConfig("regionmapping"), ...) { 
+  extraArgs <- list(...)
+  if (length(extraArgs) > 0) stop("Not yet supported!")
+
+  argumentValues <- c(as.list(environment()), list(...)) # capture arguments for logging
+  startinfo <- toolstartmessage("bundleCompile", argumentValues, 0)
+  
+  setConfig(regionmapping = regionmapping, forcecache = TRUE, .local = TRUE)
+  bundle <- normalizePath(bundle)
+  
+  with_tempdir({
+    untar(bundle, exdir="bundle")
+    cfg <- read_yaml("bundle/config.yml")
+    retrieveData(model = cfg$model, rev = cfg$rev, dev = cfg$dev, 
+                 cachetype = "def", cachefolder = "./bundle")
+  })
+  
+  toolendmessage(startinfo)
+}
