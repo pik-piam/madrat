6642f0a192e73dde21d6bdcddd269f50fb4ba1cf
David Klein <dklein@pik-potsdam.de>
2022-04-29 10:23:03 +0200

    Remove to nolint statements

--- R/toolFillYears.R
+++ R/toolFillYears.R
@@ -5,13 +5,13 @@
 #' @param years vector of years as digits or in mag year format
 #' @return MAgPIE object with completed time dimensionality.
 #' @author Kristine Karstens
-#' 
+#'
 #' @importFrom magclass time_interpolate
 #' @export
 
 toolFillYears<-function(x, years){
 
-  if(class(years)=="character") years <- as.numeric(substring(years, 2))
+  if(inherits(years, "character")) years <- as.numeric(substring(years, 2))
   out                        <- new.magpie(getCells(x), years, getNames(x), sets=getSets(x))
 
   # Fill in x contained years to out, if requested by argument years
