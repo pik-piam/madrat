f3131fe65a409ee3777e0b90f24635a7cbd01a2c
Michaja Pehl <pehl@pik-potsdam.de>
2024-10-23 10:32:47 +0200

    extend installedMadratUniverse() to include all packages with madrat as a dependency

--- R/installedMadratUniverse.R
+++ R/installedMadratUniverse.R
@@ -1,13 +1,14 @@
 #' installedMadratUniverse
 #'
-#' Returns a name vector of installed packages which
-#' supposedly belong to the madrat universe.
-#' They are currently derived as the union of
-#' all loaded madrat packages and all packages
-#' with a name starting with "mr" or "ms"
-#' (as the usual indicator for madrat-packages and
-#' madrat-support-packages).
+#' Returns a name vector of installed packages which supposedly belong to the
+#' madrat universe.   They are currently derived as the union of
+#' - all packages registered under `getConfig("packages")`,
+#' - all packages with a name starting with "mr" or "ms" (as the usual
+#'   indicator for madrat-packages and madrat-support-packages), and
+#' - all packages having `madrat` as either a `Depends` or `Imports`
+#'   dependency.
 #'
+#' @md
 #' @return A name vector of installed packages which supposedly belong
 #' to the madrat universe
 #' @author Jan Philipp Dietrich
@@ -19,5 +20,10 @@
 #' @export
 #'
 installedMadratUniverse <- function() {
- return(robustSort(union(getConfig("packages"), grep("^m[rs]", rownames(installed.packages()), value = TRUE))))
+  pkgs <- installed.packages()
+  robustSort(unique(c(
+    getConfig("packages"),
+    grep("^m[rs]", rownames(pkgs), value = TRUE),
+    rownames(pkgs)[  grepl("\\bmadrat\\b", pkgs[,"Depends"])
+                   | grepl("\\bmadrat\\b", pkgs[,"Imports"])])))
 }
