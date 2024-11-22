1073a9d07746411e694c99c5099c873ead4db1f8
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2024-11-22 12:32:35 +0100

    linter

--- R/wrappedReadSource.R
+++ R/wrappedReadSource.R
@@ -7,7 +7,7 @@
 #' rest of the function name determines the 'type'.
 #' @param correct A function to correct the data. Optional. Function name must start with 'correct'
 #' and end with the type, same as for read.
-#' @param covert A function to convert the data (putting it at country level). Optional. Function 
+#' @param covert A function to convert the data (putting it at country level). Optional. Function
 #' name must start with 'convert' and end with the type, same as for read.
 #' @inheritParams readSource
 #' @inherit readSource return
@@ -17,19 +17,25 @@ wrappedReadSource <- function(read, correct = NULL, convert = NULL, subtype = NU
 
   # get string/bool from function(s) for use in readSource
   .getConvert <- function(convert, correct) {
-    if (!is.null(convert)) {do_convert <- TRUE}
-    else if (!is.null(correct)) {do_convert <- "onlycorrect"}
-    else {do_convert <- FALSE}
-    return(do_convert)
+    if (!is.null(convert)) {
+      convertArg <- TRUE
+    } else if (!is.null(correct)) {
+      convertArg <- "onlycorrect"
+    } else {
+      convertArg <- FALSE
+    }
+    return(convertArg)
   }
 
   # get default subtype argument if available, otherwise NULL
-  if (is.null(subtype)) {subtype <- formals(eval(read))[["subtype"]]}
+  if (is.null(subtype)) {
+    subtype <- formals(eval(read))[["subtype"]]
+  }
 
   # conversions
   type <- sub("^.*?read", "", deparse(substitute(read)))
-  do_convert <- .getConvert(convert=convert, correct=correct)
+  convertArg <- .getConvert(convert = convert, correct = correct)
 
   # call readSource with original interface
-  readSource(type=type, subtype=subtype, subset=subset, convert=do_convert)
+  readSource(type = type, subtype = subtype, subset = subset, convert = convertArg)
 }
