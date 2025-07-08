4a00cb16d5f56c2ad99077052dded3b553ebabfd
Patrick R <hi@patrickrein.de>
2025-07-08 13:51:07 +0200

    Adds first version of diffOutput function

--- R/toolCompareStatusLogStatistics.R
+++ R/toolCompareStatusLogStatistics.R
@@ -5,15 +5,16 @@
 #' @param newLogPath 
 #' @author Patrick Rein
 #' @export
-toolStatisticsCompareStatusLogs <- function(oldLogPath, newLogPath) {
+toolStatisticsCompareStatusLogs <- function(oldLogPath, newLogPath,
+                                            sections = c("statistics", "changedCalls", "addedCalls", "removedCalls")) {
   # Why is this a custom diff implementation instead of textual diff?
   # -> Ordering in status.log can change in all kinds of ways and is not important.
   #    Similar vein: statistics log entries have identity and should only be compared
   #    based on their content.
   oldStatusLog <- yaml::read_yaml(oldLogPath)
-  newStatusLog <- yaml::read_yaml(newStatusLog)
+  newStatusLog <- yaml::read_yaml(newLogPath)
   diff <- .compareStatusLogsStatistics(oldStatusLog, newStatusLog)
-  .outputDiff(diff)
+  return(.renderDiff(oldStatusLog, newStatusLog, diff, sections = sections))
 }
 
 .compareStatusLogsStatistics <- function(oldLog, newLog) {
@@ -68,7 +69,8 @@ toolStatisticsCompareStatusLogs <- function(oldLogPath, newLogPath) {
   # detect changed entries
   entryChanges <- lapply(sameEntriesNames, function(entryName) {
     if (!.entriesSame(oldEntries[[entryName]], newEntries[[entryName]])) {
-      list(oldEntries[[entryName]], newEntries[[entryName]])
+      list(old = oldEntries[[entryName]],
+           new = newEntries[[entryName]])
     } else {
       NULL
     }
@@ -98,6 +100,76 @@ toolStatisticsCompareStatusLogs <- function(oldLogPath, newLogPath) {
   return(typeof(oldEntry) == typeof(newEntry) && oldEntry == newEntry)
 }
 
-.outputDiff <- function(diffList) {
-  return("TODO")
+#' Renders a list of differences based on the relevance.
+#' The reasoning is that changed entries should come first,
+#' as they can point out subtle problems.
+#' Added or removed calls or entries are less important, as they
+#' require explicit edits to the pipeline to occur.
+#' 1. Changed entries
+#' 2. Changed calls
+#'   2.1. Added entries
+#'   2.2. Removed entries
+#' 3. Added calls
+#' 4. Removed calls
+
+.renderDiff <- function(oldLog, newLog, diffList,
+                        sections = c("statistics", "changedCalls", "addedCalls", "removedCalls")) {
+  output <- list()
+
+  # changed entries
+  if ("statistics" %in% sections) {
+    changedEntriesCalls <-
+      diffList[["changedCalls"]][vapply(diffList[["changedCalls"]], 
+                                        function(l) length(l$changedEntries) > 0, logical(1))]
+    changedEntries <- lapply(changedEntriesCalls, function(aCall) {
+      lapply(aCall$changedEntries, function(oldNewEntry) {
+        if (is.null(names(oldNewEntry[["old"]][["data"]]))) {
+          changedNames <- 1
+        } else {
+          changedNames <- Filter(function(name) {
+            !identical(oldNewEntry[["old"]][["data"]][[name]], oldNewEntry[["new"]][["data"]][[name]])
+          },
+          names(oldNewEntry[["old"]][["data"]]))
+        }
+
+        list("statistic" = oldNewEntry[["old"]][["statistic"]],
+             "from" = oldNewEntry[["old"]][["data"]][changedNames],
+             "to.." = oldNewEntry[["new"]][["data"]][changedNames])
+      })
+    })
+    changedEntries <- c(list(comment = "These statistics have changed from the old to the new log."),
+                        changedEntries)
+    output <- c(output, list("Changed Statistics" = changedEntries))
+  }
+
+  # added/removed entries
+  if ("changedCalls" %in% sections) {
+    addedEntries <- lapply(diffList[["changedCalls"]], function(aCall) {
+      aCall[["addedEntries"]]
+    })
+    addedEntries <- Filter(function(l) length(l) > 0, addedEntries)
+    removedEntries <- lapply(diffList[["changedCalls"]], function(aCall) {
+      aCall[["removedEntries"]]
+    })
+    removedEntries <- Filter(function(l) length(l) > 0, removedEntries)
+    addRemoveEntries <- list("comment" = "These calls have changes beyond changed statistics.",
+                             "Added Entries" = addedEntries, 
+                             "Removed Entries" = removedEntries)
+    output <- c(output, list("Changed Calls" = addRemoveEntries))
+  }
+
+  # added/removed calls
+  if ("addedCalls" %in% sections) {
+    addedCalls <- c(list("# These calls were added"),
+                    diffList[["addedCalls"]])
+    output <- c(output, list("Added Calls" = addedCalls))
+  }
+
+  if ("removedCalls" %in% sections) {
+    removedCalls <- c(list("# These calls were removed"),
+                      diffList[["removedCalls"]])
+    output <- c(output, list("Removed Calls" = removedCalls))
+  }
+
+  return(yaml::as.yaml(output))
 }
