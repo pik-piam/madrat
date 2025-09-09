352f52d42ff36bc39ab4f7bf2eb9f2672a9f9314
Patrick Rein <patrick.rein@pik-potsdam.de>
2025-09-09 10:40:16 +0200

    Adadpts test for fingerprint cache file message, improves formating of message

--- R/fingerprint.R
+++ R/fingerprint.R
@@ -143,7 +143,7 @@ fingerprintFiles <- function(paths) {
         }
       }, silent = TRUE)
       if (inherits(tryResult, "try-error")) {
-        vcat(1, "Ignoring corrupt hashCacheFile (", hashCacheFile, "): ", as.character(tryResult))
+        vcat(1, paste0("Ignoring corrupt hashCacheFile (", hashCacheFile, "): ", as.character(tryResult)))
         filesCache <- NULL
       }
     } else {
