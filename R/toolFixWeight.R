5baa33eca22697b41d3c45667284d4174926f9de
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-11-06 10:42:53 +0100

    export toolFixWeight

--- R/toolFixWeight.R
+++ R/toolFixWeight.R
@@ -1,9 +1,9 @@
 # instead of adding a small number (like 10^-30) to each entry in weight,
 # set only those weights to a small number where otherwise data would be lost/
 # total sum of the (dis)aggregated object would be different from the original
+#' @export
 toolFixWeight <- function(weight, mapping, from, to, dim) {
   # TODO documentation
-  # TODO export?
   stopifnot(weight >= 0)
   weightSum <- toolAggregate(weight, mapping, from = from, to = to, dim = dim)
   weightSum[weightSum > 0] <- -Inf
