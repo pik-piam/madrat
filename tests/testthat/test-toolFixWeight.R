5d8b1c42a67213e7f504ff72403571b16ecaaadd
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-11-11 11:16:43 +0100

    add test

--- tests/testthat/test-toolFixWeight.R
+++ tests/testthat/test-toolFixWeight.R
@@ -1,4 +1,4 @@
-test_that("toolFixWeight works", {
+test_that("toolFixWeight works with one dimensional weight", {
   x <- new.magpie(c("A", "B"), fill = 100)
   rel <- data.frame(from = c("A", "A", "B", "B"),
                     to = c("A1", "A2", "B1", "B2"))
@@ -10,5 +10,20 @@ test_that("toolFixWeight works", {
   expected[c("A1", "A2"), , ] <- 10^-30
   expected["B1", , ] <- 1
   expected["B2", , ] <- 0
+  expect_identical(fixedWeight, expected)
+})
+
+test_that("toolFixWeight works with three dimensional weight", {
+  x <- new.magpie(c("A", "B"), c("CC", "DD"), c("EEE", "FFF"), fill = 100)
+  rel <- data.frame(from = c("A", "A", "B", "B"),
+                    to = c("A1", "A2", "B1", "B2"))
+  weight <- new.magpie(rel$to, getItems(x, 2), getItems(x, 3), fill = 0)
+  weight["B1", "CC", "EEE"] <- 1
+
+  fixedWeight <- toolFixWeight(weight, rel, from = "from", to = "to", dim = 1)
+  expected <- weight
+  expected[] <- 10^-30
+  expected["B1", "CC", "EEE"] <- 1
+  expected["B2", "CC", "EEE"] <- 0
   expect_identical(fixedWeight, expected)
 })
