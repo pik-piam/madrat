ba8d4cdc0423905900051e00e589364521cc1041
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-11-13 15:13:42 +0100

    simplify tests

--- tests/testthat/test-toolFixWeight.R
+++ tests/testthat/test-toolFixWeight.R
@@ -1,11 +1,10 @@
 test_that("toolFixWeight works with one dimensional weight", {
-  x <- new.magpie(c("A", "B"), fill = 100)
-  rel <- data.frame(from = c("A", "A", "B", "B"),
+  map <- data.frame(from = c("A", "A", "B", "B"),
                     to = c("A1", "A2", "B1", "B2"))
-  weight <- new.magpie(rel$to, fill = 0)
+  weight <- new.magpie(map$to, fill = 0)
   weight["B1", , ] <- 1
 
-  fixedWeight <- toolFixWeight(weight, rel, dim = 1)
+  fixedWeight <- toolFixWeight(weight, map, dim = 1)
   expected <- new.magpie(c("A1", "A2", "B1", "B2"))
   expected[c("A1", "A2"), , ] <- 10^-30
   expected["B1", , ] <- 1
@@ -14,13 +13,12 @@ test_that("toolFixWeight works with one dimensional weight", {
 })
 
 test_that("toolFixWeight works with three dimensional weight", {
-  x <- new.magpie(c("A", "B"), c("CC", "DD"), c("EEE", "FFF"), fill = 100)
-  rel <- data.frame(from = c("CC", "CC", "CC", "DD", "DD"),
+  map <- data.frame(from = c("CC", "CC", "CC", "DD", "DD"),
                     to = c("CC1", "CC2", "CC3", "DD1", "DD2"))
-  weight <- new.magpie(getItems(x, 1), rel$to, getItems(x, 3), fill = 0)
+  weight <- new.magpie(c("A", "B"), map$to, c("EEE", "FFF"), fill = 0)
   weight["B", "CC3", "EEE"] <- 1
 
-  fixedWeight <- toolFixWeight(weight, rel, dim = 2)
+  fixedWeight <- toolFixWeight(weight, map, dim = 2)
   expected <- weight
   expected[] <- 10^-30
   expected["B", "CC3", "EEE"] <- 1
@@ -29,13 +27,12 @@ test_that("toolFixWeight works with three dimensional weight", {
 })
 
 test_that("toolFixWeight works with subdims", {
-  x <- new.magpie(c("A", "B"), c("X.CC", "X.DD"), c("EEE", "FFF"), fill = 100)
-  rel <- data.frame(from = c("CC", "CC", "CC", "DD", "DD"),
+  map <- data.frame(from = c("CC", "CC", "CC", "DD", "DD"),
                     to = c("CC1", "CC2", "CC3", "DD1", "DD2"))
-  weight <- new.magpie(getItems(x, 1), paste0("X.", rel$to), getItems(x, 3), fill = 0)
+  weight <- new.magpie(c("A", "B"), paste0("X.", map$to), c("EEE", "FFF"), fill = 0)
   weight["B", "CC3", "EEE"] <- 1
 
-  fixedWeight <- toolFixWeight(weight, rel, dim = 2.2)
+  fixedWeight <- toolFixWeight(weight, map, dim = 2.2)
   expected <- weight
   expected[] <- 10^-30
   expected["B", "CC3", "EEE"] <- 1
@@ -52,7 +49,7 @@ test_that("toolFixWeight can handle large objects", {
   map <- data.frame(from = substr(to, 1, 1), to = to)
 
   weight <- new.magpie(to, fill = 0)
-  idx <- as.integer(runif(20, 1, ncells(weight)))
+  idx <- sample(seq_len(ncells(weight)), 20)
   weight[idx, , ] <- runif(length(idx))
 
   timed <- system.time(toolFixWeight(weight, map, dim = 1))
