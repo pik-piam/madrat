3fcb159794e1b618f1e60820dc4640d81f6574a8
Patrick R <hi@patrickrein.de>
2026-02-12 14:05:27 +0100

    Adds a short documentation comment to toolExpandRel

--- R/toolAggregate.R
+++ R/toolAggregate.R
@@ -437,6 +437,16 @@ toolGetAggregationMatrix <- function(rel, from = NULL, to = NULL, items = NULL, 
   return(m)
 }
 
+#' toolExpandRel
+#'
+#' Expands aggregation matrix to full items for matrices that only
+#' contain subdim items in from/to.
+#'
+#' Works by first generating all possible from/to items, then sub-setting
+#' to only the ones that are actually valid, i.e. from-items that appear in the
+#' original data, and to-items that only contain sub-items that also occur in
+#' the from-items, so an aggregated item should only contain sub-items that
+#' were also present in all from-items it was aggregated from.
 toolExpandRel <- function(rel, x, dim) {
   names <- getItems(x, round(floor(dim)))
 
